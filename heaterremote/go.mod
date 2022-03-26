a4b08905830eab5b4af639627dbf6b5db6eb933f
Octopus Octopus <oct2pus@jade.moe>
2022-03-26 12:29:30 -0500

    iterates on heater remote

--- heaterremote/go.mod
+++ heaterremote/go.mod
@@ -2,9 +2,10 @@ module git.jade.moe/bits-and-pieces/heaterremote
 
 go 1.17
 
+require github.com/deadsy/sdfx v0.0.0-20220326004018-11337467a689
+
 require (
 	github.com/ajstarks/svgo v0.0.0-20200725142600-7a3c8b57fecb // indirect
-	github.com/deadsy/sdfx v0.0.0-20220326004018-11337467a689 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/llgcode/draw2d v0.0.0-20200930101115-bfaf5d914d1e // indirect
 	github.com/yofu/dxf v0.0.0-20190710012328-5a6d1e83f16c // indirect
--- heaterremote/main.go
+++ heaterremote/main.go
@@ -13,12 +13,13 @@ import (
 7 mm - remote depth
 22.4 mm - remote length
 39.5 mm - remote width
-lets use 0.6 mm for size of outline
+lets use 1.2 mm for size of outline
+main body needs to be a little bigger
 */
 
 func grip() (sdf.SDF3, error) {
-	gripDimensions := []sdf.V2{{X: 0, Y: 0}, {X: 40.1, Y: 0}, {X: 40.1, Y: 13.6}, {X: 0, Y: 13.6}}
-	cutoutDimensions := []sdf.V2{{X: 0, Y: 0.6}, {X: 40.1, Y: 0.6}, {X: 40.1, Y: 13}, {X: 0, Y: 13}}
+	gripDimensions := []sdf.V2{{X: 0, Y: 0}, {X: 40.7, Y: 0}, {X: 40.7, Y: 13.6}, {X: 0, Y: 13.6}}
+	cutoutDimensions := []sdf.V2{{X: 0, Y: 1.2}, {X: 40.7, Y: 1.2}, {X: 40.7, Y: 12.4}, {X: 0, Y: 12.4}}
 	grip, err := sdf.Polygon2D(gripDimensions)
 	if err != nil {
 		return nil, err
@@ -28,8 +29,8 @@ func grip() (sdf.SDF3, error) {
 		return nil, err
 	}
 	gripHands := sdf.Difference2D(grip, cutout)
-	grip3D := sdf.Extrude3D(grip, 0.6)
-	gripHands3D := sdf.Extrude3D(gripHands, 10)
+	grip3D := sdf.Extrude3D(grip, 1.2)
+	gripHands3D := sdf.Extrude3D(gripHands, 11.2)
 	grip3D = sdf.Transform3D(grip3D, sdf.Translate3d(sdf.V3{X: 0, Y: 0, Z: -5}))
 	grip3D = sdf.Union3D(grip3D, gripHands3D)
 	return grip3D, err
